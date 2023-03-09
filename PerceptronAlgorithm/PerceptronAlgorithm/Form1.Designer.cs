8b5feecc8f2fc55a9169dc6020d7032a989c7425
Kid Omar Costelo <kidomar.costelo@cit.edu>
2023-03-09 12:37:59 +0800

    Develop Algorithm

---
+++ PerceptronAlgorithm/PerceptronAlgorithm/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace PerceptronAlgorithm
+{
+    partial class Perceptron
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Perceptron));
+            this.tbl = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbl2 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbl
+            // 
+            this.tbl.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.tbl, "tbl");
+            this.tbl.ForeColor = System.Drawing.Color.Black;
+            this.tbl.Name = "tbl";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // textBox4
+            // 
+            resources.ApplyResources(this.textBox4, "textBox4");
+            this.textBox4.Name = "textBox4";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // tbl2
+            // 
+            this.tbl2.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.tbl2, "tbl2");
+            this.tbl2.ForeColor = System.Drawing.Color.Black;
+            this.tbl2.Name = "tbl2";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            this.label10.UseMnemonic = false;
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label12);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBox3);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Perceptron
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tbl2);
+            this.Controls.Add(this.tbl);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Perceptron";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TableLayoutPanel tbl2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PerceptronAlgorithm/PerceptronAlgorithm/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Documents;
+using System.Windows.Forms;
+
+namespace PerceptronAlgorithm
+{
+    public partial class Perceptron : Form
+    {
+        private List<int> x1 = new List<int>() { 1, 1, -1, -1 };
+        private List<int> x2 = new List<int>() { 1, -1, 1, -1 };
+        private List<int> t = new List<int>() { 1, -1, -1, -1 };
+
+        private List<float> arrBias = new List<float>();
+        private List<float> arrW1 = new List<float>();
+        private List<float> arrW2 = new List<float>();
+        private List<int> arrY = new List<int>();
+
+        private float w1, w2, bias, lrate;
+        
+        public Perceptron()
+        {
+            InitializeComponent();
+
+            // Initialize data in the table
+            tbl.Controls.Add(new Label() { Text = "x1"}, 0, 0);
+            tbl.Controls.Add(new Label() { Text = "x2"}, 1, 0);
+            tbl.Controls.Add(new Label() { Text = "t"}, 2, 0);
+
+            
+            FillInputData();
+
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "1";
+        }
+
+        private void FillInputData()
+        {
+            for (int i = 1; i < 5; i++)
+            {
+
+                tbl.Controls.Add(new Label() { Text = "" + x1[i-1] }, 0, i);
+                tbl.Controls.Add(new Label() { Text = "" + x2[i-1] }, 1, i);
+                tbl.Controls.Add(new Label() { Text = "" + t[i-1] }, 2, i);
+            }
+        }
+
+        private void Algorithm(float weight1, float weight2, float b, float lr)
+        {
+            ClearArray();
+            tbl2.Controls.Clear();
+
+
+            tbl2.Controls.Add(new Label() { Text = "x1" }, 0, 0);
+            tbl2.Controls.Add(new Label() { Text = "x2" }, 1, 0);
+            tbl2.Controls.Add(new Label() { Text = "t" }, 2, 0);
+            tbl2.Controls.Add(new Label() { Text = "y" }, 3, 0);
+            tbl2.Controls.Add(new Label() { Text = "newW1" }, 4, 0);
+            tbl2.Controls.Add(new Label() { Text = "newW2" }, 5, 0);
+            tbl2.Controls.Add(new Label() { Text = "newBias" }, 6, 0);
+
+
+            // Step 1
+            // Initialize all weights, bias and learning rate
+            w1 = weight1; w2 = weight2; bias = b; lrate = lr;
+            float yin;
+            int y;
+
+            
+
+            // Step 2 
+            // Calculate new input
+            for (int i = 0; i < 4; i++)
+            {
+                // Sigmoid
+                //float num = (x1[i] * w1) + (x2[i] * w2) + bias;
+                yin = (x1[i] * w1) + (x2[i] * w2) + bias;
+                //arrYin.Add(yin);
+
+                y = SigmoidFunc(yin);
+                arrY.Add(y);
+
+                // Step 4 update weights
+                
+                    // newwegiht = oldweight + learningrate * t[i] * x[i]
+                    w1 = w1 + (lrate * t[i] * x1[i]);
+                    w2 = w2 + (lrate * t[i] * x2[i]);
+
+                    arrW1.Add(w1);
+                    arrW2.Add(w2);
+
+                    // new bias
+                    bias = bias + (lrate * t[i]);
+                    arrBias.Add(bias);
+                
+
+                tbl2.Controls.Add(new Label() { Text = "" + x1[i] }, 0, i + 1);
+                tbl2.Controls.Add(new Label() { Text = "" + x2[i] }, 1, i + 1);
+                tbl2.Controls.Add(new Label() { Text = "" + t[i] }, 2, i + 1);
+                tbl2.Controls.Add(new Label() { Text = "" + arrY[i] }, 3, i + 1);
+                tbl2.Controls.Add(new Label() { Text = "" + arrW1[i] }, 4, i + 1);
+                tbl2.Controls.Add(new Label() { Text = "" + arrW2[i] }, 5, i + 1);
+                tbl2.Controls.Add(new Label() { Text = "" + arrBias[i] }, 6, i + 1);
+
+                tbl2.Update();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tbl2.Controls.Clear();
+            ClearArray();
+            label10.Text = "" ;
+            label11.Text = "" ;
+            label12.Text = "" ;
+            label9.Text = "Iteration:";
+
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+            textBox4.Text = "1";
+        }
+
+        private int SigmoidFunc(float num)
+        {
+            if (num > 0)
+            {
+                return 1;
+            }
+            if (num < 0)
+            {
+                return -1;
+            }
+            return 0;
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            w1 = float.Parse(textBox1.Text);
+            w2 = float.Parse(textBox2.Text);
+            bias = float.Parse(textBox3.Text);
+            lrate = float.Parse(textBox4.Text);
+            int count = 0;
+
+            label9.Text = "Iteration:";
+
+            while (true)
+            {
+                count++;
+                label9.Text = $"Iteration: {count}";
+
+                Algorithm(w1, w2, bias, lrate);
+
+                label10.Text = "" + w1;
+                label11.Text = "" + w2;
+                label12.Text = "" + bias;
+
+                if (t.SequenceEqual(arrY))
+                {
+                    break;
+                }
+
+            }
+
+            textBox1.Text = "" + w1;
+            textBox2.Text = "" + w2;
+            textBox3.Text = "" + bias;
+        }
+
+        private void ClearArray()
+        {
+            arrBias.Clear();
+            arrW1.Clear();
+            arrW2.Clear();
+            arrY.Clear();
+        }
+    }
+}
